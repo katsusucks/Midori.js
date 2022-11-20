b1a26fdbcd019cd0e071d0f8e4e4ec2c5e0c6f7f
Momo <84060730+MomoCodesStuff@users.noreply.github.com>
2022-11-20 18:22:22 +0545

    Updated index.js to v4.0.0

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const { Plugins } = require("./src/index.js")
+const { Plugins } = require ("./src/index.js")
+
+
+console.log('\033[36mmidori.js is trying to be packed!');
+
+
+console.log('\033[92mDiscord server : https://discord.gg/xPURT2B3sA'); setTimeout(() => { 
   
+console.log('\033[32mmidori.js is now sucessfully packed!'); }, 1000);
+
+
+
 module.exports = {
   Plugins
 };
 
-//Load
-console.log('\033[32mmidori.js is successfully packed!');

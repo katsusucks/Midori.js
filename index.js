2754293fa42a5cb8ae4d4f1248f6b9de1baceb16
Kshitiz <84060730+KshitizTrashh@users.noreply.github.com>
2022-11-23 21:59:46 +0545

    Updated index.js with smol change

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ console.log('\033[92mDiscord server : https://discord.gg/xPURT2B3sA'); setTimeou
 console.log('\033[32mmidori.js is now sucessfully packed!'); }, 1500);
 
 
-
 module.exports = {
   Plugins
 };

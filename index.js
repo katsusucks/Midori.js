508ed6e0bb4ca7232080d5aecdbb6a3157a355a9
Kshitiz <84060730+KshitizTrashh@users.noreply.github.com>
2023-07-01 08:26:02 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const { Plugins } = require ("./src/index.js")
 
-
 console.log('\033[36mmidori.js is trying to be packed!');
 
-
 console.log('\033[92mDiscord server : https://discord.gg/xPURT2B3sA'); setTimeout(() => { 
-  
-console.log('\033[32mmidori.js is now sucessfully packed!'); }, 1500);
 
+console.log('\033[32mmidori.js is now sucessfully packed!'); }, 1000);
 
 module.exports = {
   Plugins
 };
+
